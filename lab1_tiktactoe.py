66fa6aa9a160cdc88f91bcad060c3fa10d339122
Roshan <84079544+Roshan45r@users.noreply.github.com>
2023-01-27 23:07:59 +0530

    Update lab1_tiktactoe.py

--- lab1_tiktactoe.py
+++ lab1_tiktactoe.py
@@ -1,179 +1,164 @@
-# Python3 program to find the next optimal move for a player
-player, opponent = 'x', 'o'
-
-# This function returns true if there are moves
-# remaining on the board. It returns false if
-# there are no moves left to play.
-def isMovesLeft(board) :
-
-    for i in range(3) :
-   	 for j in range(3) :
-   		 if (board[i][j] == '_') :
-   			 return True
-    return False
-
-# This is the evaluation function as discussed
-# in the previous article ( http://goo.gl/sJgv68 )
-def evaluate(b) :
-
-    # Checking for Rows for X or O victory.
-    for row in range(3) :    
-   	 if (b[row][0] == b[row][1] and b[row][1] == b[row][2]) :    
-   		 if (b[row][0] == player) :
-   			 return 10
-   		 elif (b[row][0] == opponent) :
-   			 return -10
-
-    # Checking for Columns for X or O victory.
-    for col in range(3) :
-    
-   	 if (b[0][col] == b[1][col] and b[1][col] == b[2][col]) :
-   	 
-   		 if (b[0][col] == player) :
-   			 return 10
-   		 elif (b[0][col] == opponent) :
-   			 return -10
-
-    # Checking for Diagonals for X or O victory.
-    if (b[0][0] == b[1][1] and b[1][1] == b[2][2]) :
-    
-   	 if (b[0][0] == player) :
-   		 return 10
-   	 elif (b[0][0] == opponent) :
-   		 return -10
-
-    if (b[0][2] == b[1][1] and b[1][1] == b[2][0]) :
-    
-   	 if (b[0][2] == player) :
-   		 return 10
-   	 elif (b[0][2] == opponent) :
-   		 return -10
-
-    # Else if none of them have won then return 0
-    return 0
-
-# This is the minimax function. It considers all
-# the possible ways the game can go and returns
-# the value of the board
-def minimax(board, depth, isMax) :
-    score = evaluate(board)
-
-    # If Maximizer has won the game return his/her
-    # evaluated score
-    if (score == 10) :
-   	 return score
-
-    # If Minimizer has won the game return his/her
-    # evaluated score
-    if (score == -10) :
-   	 return score
-
-    # If there are no more moves and no winner then
-    # it is a tie
-    if (isMovesLeft(board) == False) :
-   	 return 0
-
-    # If this maximizer's move
-    if (isMax) :    
-   	 best = -1000
-
-   	 # Traverse all cells
-   	 for i in range(3) :   	 
-   		 for j in range(3) :
-   		 
-   			 # Check if cell is empty
-   			 if (board[i][j]=='_') :
-   			 
-   				 # Make the move
-   				 board[i][j] = player
-
-   				 # Call minimax recursively and choose
-   				 # the maximum value
-   				 best = max( best, minimax(board,
-   										 depth + 1,
-   										 not isMax) )
-
-   				 # Undo the move
-   				 board[i][j] = '_'
-   	 return best
-
-    # If this minimizer's move
-    else :
-   	 best = 1000
-
-   	 # Traverse all cells
-   	 for i in range(3) :   	 
-   		 for j in range(3) :
-   		 
-   			 # Check if cell is empty
-   			 if (board[i][j] == '_') :
-   			 
-   				 # Make the move
-   				 board[i][j] = opponent
-
-   				 # Call minimax recursively and choose
-   				 # the minimum value
-   				 best = min(best, minimax(board, depth + 1, not isMax))
-
-   				 # Undo the move
-   				 board[i][j] = '_'
-   	 return best
-
-# This will return the best possible move for the player
-def findBestMove(board) :
-    bestVal = -1000
-    bestMove = (-1, -1)
-
-    # Traverse all cells, evaluate minimax function for
-    # all empty cells. And return the cell with optimal
-    # value.
-    for i in range(3) :    
-   	 for j in range(3) :
-   	 
-   		 # Check if cell is empty
-   		 if (board[i][j] == '_') :
-   		 
-   			 # Make the move
-   			 board[i][j] = player
-
-   			 # compute evaluation function for this
-   			 # move.
-   			 moveVal = minimax(board, 0, False)
-
-   			 # Undo the move
-   			 board[i][j] = '_'
-
-   			 # If the value of the current move is
-   			 # more than the best value, then update
-   			 # best/
-   			 if (moveVal > bestVal) :   		 
-   				 bestMove = (i, j)
-   				 bestVal = moveVal
-
-    print("The value of the best Move is :", bestVal)
-    print()
-    return bestMove
-# Driver code
+def printBoard(board):
+    print(board[1] + '|' + board[2] + '|' + board[3])
+    print('-+-+-')
+    print(board[4] + '|' + board[5] + '|' + board[6])
+    print('-+-+-')
+    print(board[7] + '|' + board[8] + '|' + board[9])
+    print("\n")
 
 
+def spaceIsFree(position):
+    if board[position] == ' ':
+        return True
+    else:
+        return False
 
 
-def main():
-	board = [
-    [ '_', '_', '_' ],
-    [ '_', '_', '_' ],
-    [ '_', '_', '_'  ]
-	]
-	while isMovesLeft(board):
-    	print(board)
-    	bestMove = findBestMove(board)
-    	print("Best move ",bestMove)
-    	board[bestMove[0]][bestMove[1]]='x'
-    	print(board)
-    	print("Enter r and c of your move")
-    	r=input()
-    	c=input()
-    	board[int(r)][int(c)]='o'
-   	 
-   	 
-main()
+def insertLetter(letter, position):
+    if spaceIsFree(position):
+        board[position] = letter
+        printBoard(board)
+        if (checkDraw()):
+            print("Draw!")
+            exit()
+        if checkForWin():
+            if letter == 'X':
+                print("Bot wins!")
+                exit()
+            else:
+                print("Player wins!")
+                exit()
+
+        return
+
+
+    else:
+        print("Can't insert there!")
+        position = int(input("Please enter new position:  "))
+        insertLetter(letter, position)
+        return
+
+
+def checkForWin():
+    if (board[1] == board[2] and board[1] == board[3] and board[1] != ' '):
+        return True
+    elif (board[4] == board[5] and board[4] == board[6] and board[4] != ' '):
+        return True
+    elif (board[7] == board[8] and board[7] == board[9] and board[7] != ' '):
+        return True
+    elif (board[1] == board[4] and board[1] == board[7] and board[1] != ' '):
+        return True
+    elif (board[2] == board[5] and board[2] == board[8] and board[2] != ' '):
+        return True
+    elif (board[3] == board[6] and board[3] == board[9] and board[3] != ' '):
+        return True
+    elif (board[1] == board[5] and board[1] == board[9] and board[1] != ' '):
+        return True
+    elif (board[7] == board[5] and board[7] == board[3] and board[7] != ' '):
+        return True
+    else:
+        return False
+
+
+def checkWhichMarkWon(mark):
+    if board[1] == board[2] and board[1] == board[3] and board[1] == mark:
+        return True
+    elif (board[4] == board[5] and board[4] == board[6] and board[4] == mark):
+        return True
+    elif (board[7] == board[8] and board[7] == board[9] and board[7] == mark):
+        return True
+    elif (board[1] == board[4] and board[1] == board[7] and board[1] == mark):
+        return True
+    elif (board[2] == board[5] and board[2] == board[8] and board[2] == mark):
+        return True
+    elif (board[3] == board[6] and board[3] == board[9] and board[3] == mark):
+        return True
+    elif (board[1] == board[5] and board[1] == board[9] and board[1] == mark):
+        return True
+    elif (board[7] == board[5] and board[7] == board[3] and board[7] == mark):
+        return True
+    else:
+        return False
+
+
+def checkDraw():
+    for key in board.keys():
+        if (board[key] == ' '):
+            return False
+    return True
+
+
+def playerMove():
+    position = int(input("Enter the position for 'O':  "))
+    insertLetter(player, position)
+    return
+
+
+def compMove():
+    bestScore = -800
+    bestMove = 0
+    for key in board.keys():
+        if (board[key] == ' '):
+            board[key] = bot
+            score = minimax(board, 0, False)
+            board[key] = ' '
+            if (score > bestScore):
+                bestScore = score
+                bestMove = key
+
+    insertLetter(bot, bestMove)
+    return
+
+
+def minimax(board, depth, isMaximizing):
+    if (checkWhichMarkWon(bot)):
+        return 1
+    elif (checkWhichMarkWon(player)):
+        return -1
+    elif (checkDraw()):
+        return 0
+
+    if (isMaximizing):
+        bestScore = -800
+        for key in board.keys():
+            if (board[key] == ' '):
+                board[key] = bot
+                score = minimax(board, depth + 1, False)
+                board[key] = ' '
+                if (score > bestScore):
+                    bestScore = score
+        return bestScore
+
+    else:
+        bestScore = 800
+        for key in board.keys():
+            if (board[key] == ' '):
+                board[key] = player
+                score = minimax(board, depth + 1, True)
+                board[key] = ' '
+                if (score < bestScore):
+                    bestScore = score
+        return bestScore
+
+
+board = {1: ' ', 2: ' ', 3: ' ',
+         4: ' ', 5: ' ', 6: ' ',
+         7: ' ', 8: ' ', 9: ' '}
+
+printBoard(board)
+print("Computer goes first! Good luck.")
+print("Positions are as follow:")
+print("1, 2, 3 ")
+print("4, 5, 6 ")
+print("7, 8, 9 ")
+print("\n")
+player = 'O'
+bot = 'X'
+
+
+global firstComputerMove
+firstComputerMove = True
+
+while not checkForWin():
+    compMove()
+    playerMove()
